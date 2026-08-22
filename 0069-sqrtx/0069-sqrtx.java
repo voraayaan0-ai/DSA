75cd6db5fe8aec44c89e00acb3c4cfc3d3a65fbd
Ayaan vora <voraayaan0@gmail.com>
2026-08-23 00:25:30 +0530

    Time: 1 ms (98.35%), Space: 42.7 MB (43.01%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+    int s = 1;
+    int e = x;
+    int ans = -1;
+
+    if(x == 0){
+        return 0;
+    }
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        if(mid == x/mid){
+            return mid;
+        }
+        else if(mid > x/mid){
+            e = mid -1;
+        }
+        else{
+            ans = mid;
+            s = mid + 1;
+        }
+      }
+      return ans;
+
+    }
+}

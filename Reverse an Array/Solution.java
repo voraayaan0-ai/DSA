0fb13e56e7a1d746f6c1fc813c87964b8cc7b8cc
Ayaan vora <voraayaan0@gmail.com>
2026-08-30 17:50:33 +0530

    Create Solution.java

---
+++ Reverse an Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}

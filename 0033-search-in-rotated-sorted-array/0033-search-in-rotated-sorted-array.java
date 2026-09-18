c416aac01080ebe17bf102eb847a927abd5cf13f
Ayaan vora <voraayaan0@gmail.com>
2026-09-19 00:41:25 +0530

    Time: 0 ms (100%), Space: 43.7 MB (65.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,69 +1,78 @@
 class Solution {
-    static int pivotindex(int nums[]){
-        int n = nums.length;
+
+    static int pivotindex(int arr[]) {
         int s = 0;
-        int e = n -1;
-        int ans = -1;
-        if(nums[s]<nums[e]){
-            //no effective rotation
-            return -1;
-        }
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if(nums[mid]<=nums[n-1]){
-                //left move 
+        int e = arr.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // Case 1: mid is the peak element
+            if (mid < e && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            // Case 2: mid - 1 is the peak element
+            if (mid > s && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // Left half is sorted, pivot is on the right
+            if (arr[mid] >= arr[s]) {
+                s = mid + 1;
+            } 
+            else {
                 e = mid - 1;
             }
-            else{
-                ////nums[mid]>nums[n-1]
-                ans = mid;
-                s = mid +1;
+        }
+
+        return -1;
+    }
+
+    static int binarySearch(int[] nums, int s, int e, int target) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (target > nums[mid]) {
+                s = mid + 1;
+            } 
+            else {
+                e = mid - 1;
             }
         }
-        return ans;
-    }
-    static int binarySearch(int nums[], int s, int e, int target) {
-    int n = nums.length;
 
-    while (s <= e) {
-        int mid = s + (e - s) / 2;
-
-        if (nums[mid] == target) {
-            return mid;
-        } 
-        else if (target > nums[mid]) {
-            s = mid + 1;
-        } 
-        else {
-            e = mid - 1;
-        }
+        return -1;
     }
-    return -1;
-}
+
     public int search(int[] nums, int target) {
-        int pivotindex = pivotindex(nums);
+
+        int pivot = pivotindex(nums);
         int n = nums.length;
-        if(pivotindex == -1){
-            int ans = binarySearch(nums , 0 , n-1, target);
-            return ans ;
-        }
-        else{
-            // l1 and L2 wala logic
-             int startArray1 =0;
-        int endArray1 = pivotindex;
-        if(target>=nums[startArray1] && target<=nums[endArray1]){
-            int ans = binarySearch(nums , startArray1 , endArray1 , target);
-            return ans;
-        }
-        int startArray2 = pivotindex+1;
-        int endArray2 = n-1;
-        if(target>=nums[startArray2] && target<=nums[endArray2]){
-            int ans = binarySearch(nums , startArray2 , endArray2 , target);
-            return ans;
+
+        // Array is not rotated
+        if (pivot == -1) {
+            return binarySearch(nums, 0, n - 1, target);
         }
 
-        }
-       return -1;
+        // Search in left half
+        int s1 = 0;
+        int e1 = pivot;
 
+        if (target >= nums[s1] && target <= nums[e1]) {
+            return binarySearch(nums, s1, e1, target);
+        }
+
+        // Search in right half
+        int s2 = pivot + 1;
+        int e2 = n - 1;
+
+        if (s2 < n && target >= nums[s2] && target <= nums[e2]) {
+            return binarySearch(nums, s2, e2, target);
+        }
+
+        return -1;
     }
 }
